c07cc36ebb779351aca57c03b113b46aabb33e6d
Brian-big <briannbig@gmail.com>
2021-11-03 20:41:30 +0300

    final touches

--- app/src/main/java/compose/basics/codelab/MainActivity.kt
+++ app/src/main/java/compose/basics/codelab/MainActivity.kt
@@ -3,6 +3,7 @@ package compose.basics.codelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.spring
@@ -10,10 +11,14 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -55,26 +60,9 @@ private fun Greetings(names: List<String> = List(1000) { "$it" }) {
 }
 @Composable
 private fun Greeting(name: String) {
-    val expanded = remember { mutableStateOf(false)}
-    val extraPadding by animateDpAsState( if (expanded.value) 48.dp else 0.dp,
-        animationSpec = spring(Spring.DampingRatioMediumBouncy, stiffness = Spring.StiffnessVeryLow))
-    Surface(color = MaterialTheme.colors.primary, modifier = Modifier
-        .fillMaxWidth()
-        .padding(vertical = 4.dp, horizontal = 8.dp)) {
-        Row(modifier = Modifier.padding(24.dp)) {
-            Column(modifier = Modifier
-                .weight(1f)
-                .padding(bottom = extraPadding.coerceAtLeast(0.dp))) {
-            Text(text = "Hello,")
-            Text(name, style = MaterialTheme.typography.h4.copy(
-                fontWeight = FontWeight.ExtraBold
-            ))
-            }
-            OutlinedButton(onClick = { expanded.value = !expanded.value }) {
-                Text(if (expanded.value) "Show less" else "Show more")
-            }
-        }
-    }
+   Card(backgroundColor = MaterialTheme.colors.primary, modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)) {
+       CardContent(name = name)
+   }
 }
 
 @Preview(showBackground = true)
@@ -109,5 +97,52 @@ fun OnBoardingScreen(onContinueClicked: () -> Unit) {
 fun OnBoardingPreview() {
     ComposeBasicsCodelabTheme {
         OnBoardingScreen { }
+    }
+}
+
+@Composable
+private fun CardContent(name: String) {
+    var expanded by remember { mutableStateOf(false) }
+
+    Row(
+        modifier = Modifier
+            .padding(12.dp)
+            .animateContentSize(
+                animationSpec = spring(
+                    dampingRatio = Spring.DampingRatioMediumBouncy,
+                    stiffness = Spring.StiffnessLow
+                )
+            )
+    ) {
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                .padding(12.dp)
+        ) {
+            Text(text = "Hello, ")
+            Text(
+                text = name,
+                style = MaterialTheme.typography.h4.copy(
+                    fontWeight = FontWeight.ExtraBold
+                )
+            )
+            if (expanded) {
+                Text(
+                    text = ("Composem ipsum color sit lazy, " +
+                            "padding theme elit, sed do bouncy. ").repeat(4),
+                )
+            }
+        }
+        IconButton(onClick = { expanded = !expanded }) {
+            Icon(
+                imageVector = if (expanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+                contentDescription = if (expanded) {
+                    stringResource(R.string.show_less)
+                } else {
+                    stringResource(R.string.show_more)
+                }
+
+            )
+        }
     }
 }
