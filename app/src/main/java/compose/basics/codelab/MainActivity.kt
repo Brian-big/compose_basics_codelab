f6d7174b2cbe8fd82acc2a06fd338b6ba1524961
Brian-big <briannbig@gmail.com>
2021-11-03 18:48:45 +0300

    persisting state

--- app/src/main/java/compose/basics/codelab/MainActivity.kt
+++ app/src/main/java/compose/basics/codelab/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -30,7 +31,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 private fun MyApp() {
-    var shouldShowOnBoarding by remember { mutableStateOf(true) }
+    var shouldShowOnBoarding by rememberSaveable { mutableStateOf(true) }
 
     if (shouldShowOnBoarding) {
         OnBoardingScreen { shouldShowOnBoarding = false }
@@ -79,9 +80,6 @@ private fun DefaultPreview() {
 
 @Composable
 fun OnBoardingScreen(onContinueClicked: () -> Unit) {
-    // TODO: This state should be hoisted
-    var shouldShowOnboarding by remember { mutableStateOf(true) }
-
     Surface {
         Column(
             modifier = Modifier.fillMaxSize(),
